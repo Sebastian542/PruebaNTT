830616c8a3a6fa213d6ab931e0e2c5129facf83e
Juan Sebastian Muñoz Peñuela <84586099+Sebastian542@users.noreply.github.com>
2025-04-28 09:17:20 -0500

    Modificacion cor

--- backend/demo_nttdata/src/main/java/com/clientservice/config/CorsConfig.java
+++ backend/demo_nttdata/src/main/java/com/clientservice/config/CorsConfig.java
@@ -1,26 +1,3 @@
-/**
- * -------------------------------------------------------------------
- *?Archivo: CorsConfig.java
- *?Autor: Daniel Alejandro Palma Garcia
- *?Correo: dannypalmadev@gmail.com
- *?Fecha: Octubre 2024
- *?Empresa: NTT Data
- *
- *?Descripción:
- * Este archivo contiene la configuración global de CORS para la
- * aplicación backend. Permite solicitudes desde el 
- * frontend que corre en http://localhost:4200.
- *
- * Esta configuración permite compartir recursos entre dominios,
- * habilitando el intercambio de datos entre el frontend (Angular)
- * y el backend (Spring Boot).
- *
- * - Permite solicitudes de origen http://localhost:4200
- * - Métodos permitidos: GET, POST, PUT, DELETE, OPTIONS
- * - Se permiten todos los headers
- * - Se permite el uso de credenciales
- * -------------------------------------------------------------------
- */
 
 package com.clientservice.config;
 
